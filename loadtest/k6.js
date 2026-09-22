34dc0020db7acda54fe3d8a12f122ada4c5dfc7c
Nack <nnackphatpharid@gmail.com>
2026-09-22 15:29:44 +0700

    lab3: fix k6 auth header placement

--- loadtest/k6.js
+++ loadtest/k6.js
@@ -37,8 +37,10 @@ const payload = JSON.stringify({
 
 export default function () {
   const res = http.post(__ENV.TARGET, payload, {
-    headers: { 'Content-Type': 'application/json' },
-    ...(__ENV.TOKEN ? { Authorization: `Bearer ${__ENV.TOKEN}` } : {}),
+    headers: {
+      'Content-Type': 'application/json',
+      ...(__ENV.TOKEN ? { Authorization: `Bearer ${__ENV.TOKEN}` } : {}),
+    },
   });
   latency.add(res.timings.duration);
   failures.add(res.status !== 200);
